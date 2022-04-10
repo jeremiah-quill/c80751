7b275aa8b42131148c28fa8c5917173d9b646081
Jeremiah Quill <jcq5010@gmail.com>
2022-04-10 14:38:06 -0400

    changed postMessage to async await, using previous state instead of mutating state in addNewConvo and addMessageToConversation

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import { SenderBubble, OtherUserBubble } from '.';
-import moment from 'moment';
+import React from "react";
+import { Box } from "@material-ui/core";
+import { SenderBubble, OtherUserBubble } from ".";
+import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+	const { messages, otherUser, userId } = props;
 
-  return (
-    <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format('h:mm');
+  // Bug fix: sort messages by createdAt before mapping over them
+	return (
+		<Box>
+			{messages
+				.sort((a, b) => (moment(b.createdAt) < moment(a.createdAt) ? 1 : -1))
+				.map((message) => {
+					const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
-        );
-      })}
-    </Box>
-  );
+					return message.senderId === userId ? (
+						<SenderBubble key={message.id} text={message.text} time={time} />
+					) : (
+						<OtherUserBubble
+							key={message.id}
+							text={message.text}
+							time={time}
+							otherUser={otherUser}
+						/>
+					);
+				})}
+		</Box>
+	);
 };
 
 export default Messages;
